d9edc0402a840630ad09a536f245331ca835e14c
taeilyoon <zkrxpdlf0808@gmail.com>
2026-02-06 03:09:52 +0900

    feat(alpha2-10): 아이템 시스템 설계 및 데이터 모델 구현

--- lib/item/item.dart
+++ lib/item/item.dart
@@ -1,15 +1,45 @@
-enum ItemType { Potion, Scroll, Elixir }
+enum ItemType { potion, scroll, artifact }
+
+class ItemEffect {
+  final String type;
+  final double value;
+  final int? duration; // Duration in turns, null for instant
+
+  ItemEffect({required this.type, required this.value, this.duration});
+}
 
 class Item {
   final String id;
   final String name;
+  final String description;
   final ItemType type;
-  final Map<String, dynamic> effects;
+  final List<ItemEffect> effects;
 
   Item({
     required this.id,
     required this.name,
+    required this.description,
     required this.type,
     required this.effects,
   });
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      type: ItemType.values.firstWhere(
+        (e) => e.toString().split('.').last == json['type'],
+      ),
+      effects: (json['effects'] as List)
+          .map(
+            (e) => ItemEffect(
+              type: e['type'],
+              value: e['value'].toDouble(),
+              duration: e['duration'],
+            ),
+          )
+          .toList(),
+    );
+  }
 }
