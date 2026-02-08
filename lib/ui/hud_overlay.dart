9104c0c79e88b42989544300d996a7d2b8507ac2
taeilyoon <zkrxpdlf0808@gmail.com>
2026-02-08 17:51:29 +0900

    feat(ui): HUD 물리적 분리 및 스테이지 전환 루프 구현
    - HUD를 상단(Header)과 하단(Footer) 오버레이로 분리하여 중앙 터치 간섭 제거
    - 플레이어 최하단 도달 시 스테이지 자동 전환 및 점수 가산 로직 추가
    - 조작 시스템 안정화 (PanDetector 및 widget 좌표계 적용)
    - 코드 가독성 개선을 위한 중복 로직 정리

--- lib/ui/hud_overlay.dart
+++ lib/ui/hud_overlay.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import '../game.dart';
 
-class HudOverlay extends StatefulWidget {
+class HudHeader extends StatefulWidget {
   final MyGame game;
-
-  const HudOverlay({super.key, required this.game});
+  const HudHeader({super.key, required this.game});
 
   @override
-  State<HudOverlay> createState() => _HudOverlayState();
+  State<HudHeader> createState() => _HudHeaderState();
 }
 
-class _HudOverlayState extends State<HudOverlay> {
+class _HudHeaderState extends State<HudHeader> {
   @override
   void initState() {
     super.initState();
@@ -23,188 +22,41 @@ class _HudOverlayState extends State<HudOverlay> {
     super.dispose();
   }
 
-  void _update() {
-    if (mounted) setState(() {});
-  }
+  void _update() => setState(() {});
 
   @override
   Widget build(BuildContext context) {
-    final game = widget.game;
-    return Material(
-      color: Colors.transparent,
-      // IgnorePointer set to true would block everything.
-      // We want to allow hits through the empty space but KEEP them for children.
-      // So we use a Stack without a full-screen blocking container.
-      child: SafeArea(
-        child: Stack(
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(16, 48, 16, 12),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.black.withOpacity(0.8), Colors.transparent],
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            // Header Area - Wrap in IgnorePointer(ignoring: false) to catch hits
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: IgnorePointer(
-                ignoring: false, // Catch hits for stat cards
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 12,
-                  ),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.black.withOpacity(0.7),
-                        Colors.transparent,
-                      ],
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      _buildStatCard(
-                        'HP',
-                        '${game.player.hp}',
-                        Colors.redAccent,
-                        Icons.favorite,
-                      ),
-                      _buildStatCard(
-                        'STAGE',
-                        '${game.scoreEngine.stageProgress}',
-                        Colors.blueAccent,
-                        Icons.layers,
-                      ),
-                      _buildStatCard(
-                        'SCORE',
-                        '${game.scoreEngine.total}',
-                        Colors.amber,
-                        Icons.monetization_on,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            _buildStatCard(
+              'HP',
+              '${widget.game.player.hp}',
+              Colors.redAccent,
+              Icons.favorite,
             ),
-
-            // Middle Area (Empty) - Hits will pass through to Flame by default in a Stack if no widget is here
-
-            // Combo Notification
-            if (game.comboTracker.combo > 1)
-              IgnorePointer(
-                ignoring:
-                    true, // Let combo text be purely visual, hits pass through
-                child: Align(
-                  alignment: const Alignment(0, -0.45),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        '${game.comboTracker.combo} COMBO!',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.orange,
-                          fontStyle: FontStyle.italic,
-                          shadows: [
-                            Shadow(
-                              color: Colors.black.withOpacity(0.5),
-                              offset: const Offset(2, 2),
-                              blurRadius: 4,
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 6),
-                      SizedBox(
-                        width: 120,
-                        height: 6,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(3),
-                          child: LinearProgressIndicator(
-                            value: (game.comboTracker.timeLeftMs / 3000).clamp(
-                              0.0,
-                              1.0,
-                            ),
-                            backgroundColor: Colors.black,
-                            valueColor: const AlwaysStoppedAnimation<Color>(
-                              Colors.orange,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-            // Footer Area: Inventory
-            Positioned(
-              bottom: 10,
-              left: 0,
-              right: 0,
-              child: IgnorePointer(
-                ignoring: false, // Catch hits for inventory slots
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                      colors: [
-                        Colors.black.withOpacity(0.8),
-                        Colors.transparent,
-                      ],
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text(
-                        'INVENTORY',
-                        style: TextStyle(
-                          color: Colors.white54,
-                          fontSize: 10,
-                          letterSpacing: 1.5,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(game.inventory.maxSlots, (
-                          index,
-                        ) {
-                          final item = game.inventory.slots[index];
-                          return Container(
-                            width: 48,
-                            height: 48,
-                            margin: const EdgeInsets.symmetric(horizontal: 4),
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.1),
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                color: item != null
-                                    ? Colors.white70
-                                    : Colors.white10,
-                                width: 1.2,
-                              ),
-                            ),
-                            child: item != null
-                                ? const Center(
-                                    child: Icon(
-                                      Icons.star,
-                                      color: Colors.white,
-                                      size: 24,
-                                    ),
-                                  )
-                                : null,
-                          );
-                        }),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            _buildStatCard(
+              'STAGE',
+              '${widget.game.scoreEngine.stageProgress}',
+              Colors.blueAccent,
+              Icons.layers,
+            ),
+            _buildStatCard(
+              'SCORE',
+              '${widget.game.scoreEngine.total}',
+              Colors.amber,
+              Icons.monetization_on,
             ),
           ],
         ),
@@ -225,6 +77,7 @@ class _HudOverlayState extends State<HudOverlay> {
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Row(
             mainAxisSize: MainAxisSize.min,
@@ -251,6 +104,82 @@ class _HudOverlayState extends State<HudOverlay> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class HudFooter extends StatefulWidget {
+  final MyGame game;
+  const HudFooter({super.key, required this.game});
+
+  @override
+  State<HudFooter> createState() => _HudFooterState();
+}
+
+class _HudFooterState extends State<HudFooter> {
+  @override
+  void initState() {
+    super.initState();
+    widget.game.scoreEngine.addListener(_update);
+  }
+
+  @override
+  void dispose() {
+    widget.game.scoreEngine.removeListener(_update);
+    super.dispose();
+  }
+
+  void _update() => setState(() {});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(16, 12, 16, 32),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [Colors.black.withOpacity(0.8), Colors.transparent],
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'INVENTORY',
+              style: TextStyle(
+                color: Colors.white54,
+                fontSize: 10,
+                letterSpacing: 1.5,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(widget.game.inventory.maxSlots, (index) {
+                final item = widget.game.inventory.slots[index];
+                return Container(
+                  width: 44,
+                  height: 44,
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: item != null ? Colors.white70 : Colors.white10,
+                    ),
+                  ),
+                  child: item != null
+                      ? const Icon(Icons.star, color: Colors.white, size: 20)
+                      : null,
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
